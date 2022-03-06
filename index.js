bc75e079bba182ce3ed89be840ef64eb163e40b9
jogeshgupta963 <jogeshgupta963@gmail.com>
2022-03-06 09:49:35 +0530

    frontend

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 window.onload = function () {
   var el = document.getElementById("sos-btn");
-  el.onclick = function () {
+  el.onclick = async function () {
     var my_text = prompt("Enter message");
-    if (my_text) alert(my_text);
+    // if (my_text) alert(my_text);
+    // await axios.post('localhost:1337/sms/');
+    fetch('localhost:1337/sms/')
+      .then(res => res.json)
+      .then(data => console.log(data))
   };
 };
